b2bb0bfde2bca68386ad217e6e9dc10824912af2
Roma Sokolov <sokolov.r.v@gmail.com>
2017-02-16 17:12:09 +0300

    [add] код и тесты

--- src/flock/twink.clj
+++ src/flock/twink.clj
@@ -1,3 +1,70 @@
-(ns flock.twink.core
+(ns flock.twink
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
+
+(def SEX {0 nil 1 :male 2 :female})
+
+(defn- read-dict [name]
+  (with-open [reader (io/reader (io/resource name))]
+    (let [lines (->> (line-seq reader)
+                     (map #(str/split % #",") ))
+          keys (map keyword (first lines))]
+      (doall (map #(apply assoc {} (interleave keys %)) (rest lines))))))
+
+(def names
+  (->> (read-dict "name2name.csv")
+       (reduce #(if (= "first" (:type %2))
+                  (assoc %1 (:name %2) (:name.new %2)) %1)
+               {})))
+
+
+(def sexes (atom {}))
+
+(defn- read-sexes [dict-name]
+  (into {}
+        (for [{name :name sex :sex.byname} (read-dict dict-name)
+              :let [sex-kw (Integer/parseInt sex)]
+              :when sex-kw]
+          [name sex-kw])))
+
+(def sexes-default (read-sexes "name2sex.csv"))
+
+(defn- sex? [n locale]
+  (if-let [s (@sexes locale)]
+    (get s n)
+    (let [resource-name (format "name2sex_%s.csv" (some-> locale name str/lower-case))]
+      (if (io/resource resource-name)
+        (let [loc-sexes (read-sexes resource-name)]
+          (do (swap! sexes assoc locale loc-sexes)
+              (get loc-sexes n)))
+        (sexes-default n)))))
+
+(defn capitalize-words
+  "Capitalize every word in a string"
+  [s]
+  (->> (str/split (str s) #"\b")
+       (map str/capitalize)
+       str/join))
+
+(defn- locale-to-transformer [locale]
+  (if (str/ends-with? locale "2")
+    (fn [tokens] (->> tokens
+                      (partition 2 1)
+                      (map #(str/join " " %))))
+    identity))
+
+(defn parse
+  "Пытается определить пол и имя по строке-имени. Пользуется словариком соответствий
+  имен (katya => екатерина) и полов-имен (екатерина => :female)"
+  ([n] (parse n "ru"))
+  ([n locale]
+   (when n
+     (->>
+      (re-seq #"\p{L}+" n)
+      ((locale-to-transformer locale))
+      (map str/lower-case)
+      (some
+       #(let [n2  (if (= "ru" locale) (names %) %)
+              sex (or (sex? % locale) (and n2 (sex? n2 locale)))]
+          (when (or n2 sex)
+            {:first-name (capitalize-words (or n2 %)) :sex (SEX sex)})))))))
--- test/flock/twink_test.clj
+++ test/flock/twink_test.clj
@@ -1,3 +1,58 @@
-(ns flockt.twink.core-test
+(ns flock.twink-test
   (:require [clojure.test :refer :all]
-            [flock.twink.core :refer :all]))
+            [flock.twink :refer [parse]]))
+
+(deftest parse-name-with-default-locale
+  ;; male
+  (is (= (parse "петя")
+         {:first-name "Петр" :sex :male}))
+  (is (= (parse "Александр")
+         {:first-name "Александр" :sex :male}))
+  ;; female
+  (is (= (parse "ира")
+         {:first-name "Ирина" :sex :female}))
+  (is (= (parse "евгения")
+         {:first-name "Евгения" :sex :female}))
+  ;; unisex
+  (is (= (parse "женя")
+         {:first-name "Женя" :sex nil})))
+
+(deftest parse-name-from-random-input
+  (is (= (parse "kolya1985")
+         {:first-name "Николай" :sex :male}))
+  (is (= (parse "Masha Gorbunova")
+         {:first-name "Мария" :sex :female}))
+  (is (= (parse "пётр, Абулгары  Эрнест")
+         {:first-name "Петр" :sex :male}))
+  (is (= (parse "Абулгары  Эрнест!")
+         {:first-name "Эрнест" :sex :male}))
+  (is (= (parse "natasha иванова")
+         {:first-name "Наталья" :sex :female}))
+  (is (= (parse "olga.petrov@yandex.ru")
+         {:first-name "Ольга" :sex :female}))
+  (is (= (parse "dark.imperior@live.com")
+         nil))
+  (is (= (parse "Иногда Тут Бывает Так")
+         nil)))
+
+(deftest parse-name-from-translit
+  (is (= (parse "petya")
+         {:first-name "Петр" :sex :male}))
+  (is (= (parse "olya")
+         {:first-name "Ольга" :sex :female}))
+  (is (= (parse "zhenya")
+         {:first-name "Женя" :sex nil})))
+
+(deftest parse-name-with-locale
+  (is (= (parse "Juan" "es")
+         {:first-name "Juan" :sex :male}))
+  (is (= (parse "maria" "es")
+         {:first-name "Maria" :sex :female}))
+  ;; tr
+  (is (= (parse "Nilüfer Ivanov" "tr")
+         {:first-name "Nilüfer" :sex :female}))
+  (is (= (parse "rıdvan.2001" "tr")
+         {:first-name "Rıdvan" :sex :male}))
+  ;; двойные испанские имена
+  (is (= (parse "luis pablo", "es2")
+         {:first-name "Luis Pablo" :sex :male})))
