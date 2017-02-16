affdaf4870f6c2608031cfa261baf6584a53c0e1
Roma Sokolov <sokolov.r.v@gmail.com>
2017-02-16 13:47:50 +0300

    [minor] dev/repl + renames

---
+++ dev/user/my.clj
@@ -0,0 +1,7 @@
+(ns user.my
+  (:require [flock.twink :as twink]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [clojure.test :refer [run-tests]]
+            ))
--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [])
+  :dependencies []
+  :source-paths ["src"]
+  :profiles {:repl {:source-paths ["src" "dev"]
+                    :repl-options {:init-ns user.my}
+                    :dependencies [[org.clojure/clojure "1.8.0"]
+                                   [org.clojure/tools.namespace "0.2.10"]]
+                    :injections [
+                                 (require 'clojure.tools.namespace.repl)
+                                 (require 'user.my)]}
+             :dev {:resource-paths ["resources"]
+                   :dependencies [[org.clojure/clojure "1.8.0"]
+                                  [org.clojure/tools.namespace "0.2.10"]]}})
