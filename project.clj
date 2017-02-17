bfa1cc8c8b807e5caaed5bc863e7e0cfc9ee45f4
Roma Sokolov <sokolov.r.v@gmail.com>
2017-02-17 18:33:36 +0300

    [bump] repo + version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject flock/twink "0.1.0"
-  :description "Определяем имя и пол по полному имени."
+(defproject flock/twink "0.2.0"
+  :description "Detect sex and full name from string that looks like name"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -21,6 +21,5 @@
             #"clojars" {:name "Nexus"
                         :url "http://artifacts.dev.flocktory.com:8081/nexus/content/groups/public/"
                         :repo-manager true}}
-  :deploy-repositories [["releases"
-                         {:url  "http://artifacts.dev.flocktory.com:8081/nexus/content/repositories/releases/"
-                          :sign-releases false}]])
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :sign-releases false}]])
